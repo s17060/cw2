7c50b2f61eaa7df09062a44bd037c2fee0526e85
debskimich <69129514+debskimich@users.noreply.github.com>
2020-11-16 20:05:05 +0100

    second commit

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Xml.Serialization;
 
 namespace cw2
 {
@@ -6,7 +14,60 @@ namespace cw2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worald!");
+            string inFile = "data.csv";
+            string outFile = "result.xml";
+            string dataType = "xml";
+            try
+            {
+                if(args.Length == 3)
+                {
+                    inFile = args[0];
+                    outFile = args[1];
+                    dataType = args[2];
+                }
+            } catch(Exception e)
+            {
+                if(e is FormatException)
+                {
+                    logWrite("ArgumentException", "Podana sciezka jest niepoprawna");
+                } else if(e is FileNotFoundException)
+                {
+                    logWrite("FileNotFoundException", "Plik nie istnieje");
+                }
+                
+            }
+            
+
+            using (StreamReader reader = new StreamReader(inFile))
+            using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                Student student = new Student();
+                csv.Configuration.HasHeaderRecord = false;
+                List<Student> records = csv.GetRecords<Student>().ToList();
+                
+                if(dataType.Equals("xml"))
+                {
+                    FileStream writer = new FileStream(outFile, FileMode.Create);
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+                    serializer.Serialize(writer, records);
+                    writer.Close();
+                } else if (dataType.Equals("json"))
+                {
+                    string jsonString = JsonSerializer.Serialize(records);
+                    File.WriteAllText(outFile, jsonString);
+                }
+                
+            }
+        }
+
+        public static void logWrite(string name, string desc)
+        {
+            string date = DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt");
+
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                w.WriteLine(date + " " + name + ": " + desc);
+            }
         }
     }
 }
---
+++ cw2/cw2/models/Student.cs
@@ -0,0 +1,45 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace cw2
+{
+    [Serializable]
+    public class Student
+    {
+        [Index(0)]
+        [JsonPropertyName("Imie")]
+        [XmlElement(ElementName = "Imie")]
+        public string FirstName { get; set; }
+
+        [Index(1)]
+        [JsonPropertyName("Nazwisko")]
+        [XmlElement(ElementName = "Nazwisko")]
+        public string LastName { get; set; }
+        [Index(2)]
+        [JsonPropertyName("Kierunek")]
+        [XmlElement(ElementName = "Kierunek")]
+        public string FieldOfStudy { get; set; }
+        [Index(3)]
+        [JsonPropertyName("Tryb")]
+        [XmlElement(ElementName = "Tryb")]
+        public string Mode { get; set; }
+        [Index(4)]
+        [JsonPropertyName("Numer indeksu")]
+        [XmlElement(ElementName = "Numer indeksu")]
+        public string IndexNumber { get; set; }
+        [Index(5)]
+        [JsonPropertyName("Data urodzenia")]
+        [XmlElement(ElementName = "Data urodzenia")]
+        public DateTime BirthDate { get; set; }
+        [Index(6)]
+        [JsonPropertyName("Imie matki")]
+        [XmlElement(ElementName = "Imie matki")]
+        public string MotherFirstName { get; set; }
+        [Index(7)]
+        [JsonPropertyName("Imie ojca")]
+        [XmlElement(ElementName = "Imie ojca")]
+        public string FatherFirstName { get; set; }
+    }
+}
